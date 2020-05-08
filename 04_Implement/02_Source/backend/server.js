4e25e4d01854ca830f37688330743775bd81b0a0
sang.nguyen <im.nhsang@gmail.com>
2020-05-08 22:02:43 +0700

    init be

---
+++ 04_Implement/02_Source/backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const connectDB = require('./config/db')
+
+const app = express()
+
+// Connect Datavase
+connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.send("API Running")
+})
+
+// Defined Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/auth', require('./routes/api/auth'))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
