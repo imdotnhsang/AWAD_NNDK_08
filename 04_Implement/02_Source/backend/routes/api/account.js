7f5e637c67c17c2be88c61bf3074014813b41277
sang.nguyen <im.nhsang@gmail.com>
2020-05-14 06:56:27 +0700

    update get account from me and from other bank

--- 04_Implement/02_Source/backend/routes/api/account.js
+++ 04_Implement/02_Source/backend/routes/api/account.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
 
 var { APIStatus, MakeResponse } = require("../../utils/APIStatus.js")
@@ -30,11 +31,54 @@ router.post('/', [
     }
 })
 
+router.get('/me', auth, async (req, res) => {
+    try {
+        const user = (await User.findById(req.user.id))
+
+        if(!user){
+            res.status(400).json({
+                errors:[{
+                    msg: "User not exists"
+                }]
+            })
+        }
+
+        let { full_name, default_account_id } = user
+
+        full_name = full_name.toLowerCase()
+        full_name = full_name.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a")
+        full_name = full_name.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e")
+        full_name = full_name.replace(/ì|í|ị|ỉ|ĩ/g, "i")
+        full_name = full_name.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o")
+        full_name = full_name.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u")
+        full_name = full_name.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y")
+        full_name = full_name.replace(/đ/g, "d")
+        full_name = full_name.toUpperCase()
+
+        const account = await Account.findOne({ account_id: default_account_id })
+
+        res.status(200).json({ full_name, account })
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ msg: 'Server error' })
+    }
+})
+
 router.get('/', async (req, res) => {
     try {
         const { account_id } = req.body
 
-        let full_name = (await User.findOne({ default_account_id: account_id }).select('full_name')).full_name.toLowerCase()
+        const user =  await User.findOne({ default_account_id: account_id })
+
+        if(!user){
+            res.status(400).json({
+                errors:[{
+                    msg: "User not exists"
+                }]
+            })
+        }
+
+        let full_name = user.full_name.toLowerCase()
         full_name = full_name.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
         full_name = full_name.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
         full_name = full_name.replace(/ì|í|ị|ỉ|ĩ/g, "i");
@@ -44,9 +88,7 @@ router.get('/', async (req, res) => {
         full_name = full_name.replace(/đ/g, "d");
         full_name = full_name.toUpperCase();
 
-        const account = await Account.findOne({ account_id })
-
-        res.status(200).json({ full_name, account })
+        res.status(200).json({ full_name })
     } catch (error) {
         console.error(error.message)
         res.status(500).json({ msg: 'Server error' })
