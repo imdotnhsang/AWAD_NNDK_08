dd8533e5965899f7a100dca3b02ecb7ace38195f
Leo Nguyen <hoangsang160898@gmail.com>
2020-05-20 17:42:35 +0700

    update model Transaction

--- 04_Implement/02_Source/backend/models/Transaction.js
+++ 04_Implement/02_Source/backend/models/Transaction.js
@@ -39,7 +39,7 @@ const TransactionSchema = new mongoose.Schema({
 		type: Number,
 		required: true
 	},
-	transaction_status: { // SUCCESS, FAILED, REFUND, PENDING
+	transaction_status: { // SUCCESS, FAILED, REFUND
 		type: String,
 		require: true
 	},
