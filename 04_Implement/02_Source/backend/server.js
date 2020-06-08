eff305a8f568b6693cb20382a6383b634fc5ddc8
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2020-06-08 19:59:57 +0700

    allow cors

--- 04_Implement/02_Source/backend/server.js
+++ 04_Implement/02_Source/backend/server.js
@@ -7,6 +7,27 @@ const redisClient = require('./config/redis')
 
 const app = express()
 
+app.use((req, res, next) => {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+	// Request methods you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	)
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true)
+
+	// Pass to next layer of middleware
+	next()
+})
+
 // Connect Database
 connectDB()
 
